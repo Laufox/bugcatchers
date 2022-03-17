c7fd596cbc77689c077f76b2cbb1371af8cfea7f
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-03-17 16:16:52 +0100

    commenting and cleanup

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -12,50 +12,59 @@ const playerTimeEl = document.querySelector('#userTime h5');
 let username = null;
 let room = null;
 
+// Variable for time passed before user clicks
 let timePassed = null;
+// Variable for the timer that will update and render timer to user
 let timer = null;
+// Variable for amount of milliseconds since 1 Jan 1970 at the point when the timer starts 
 let timeBeforeRound = null;
-let timeAfterRound = null;
+
+// Funtion to stop timer and calculate player click time
+const stopTimer = () => {
+	// Stop the interval timer that prints time to user
+	clearInterval(timer);
+	// Calculate how long time it took for the player to click
+	timePassed = Date.now() - timeBeforeRound;
+	console.log('It took you ' + timePassed + ' milliseconds to click');
+	// Show user the final time
+	playerTimeEl.innerText = `${Math.floor(timePassed/1000)} : ${timePassed%1000}`;
+	// Remove the click event from secretSquare
+	// --- Change from game screen to the specific square --
+	gameScreenEl.removeEventListener('click', stopTimer);
+	// Give time to server
+	socket.emit('game:round-result', timePassed, username);
+}
 
 const startTimer = () => {
-	timePassed = 0;
-	timer = setInterval( () => {
-		timePassed++;
-	}, 1 );
-}
-
-const stopTimer = () => {
-	clearInterval(timer);
-	timeAfterRound = Date.now();
-	timePassed = timeAfterRound - timeBeforeRound;
-	console.log('It took you ' + timePassed + ' milliseconds to click');
-	playerTimeEl.innerText = `${Math.floor(timePassed/1000)} : ${timePassed%1000}`;
-	gameScreenEl.removeEventListener('click', stopTimer);
-}
-
-const secretSquareClick = () => {
 	console.log("Wait time over, lets start!");
 
-	// set interval timer
+	// Reset user click time
 	timePassed = 0;
+	// Get the amount of milliseconds since 1 Jan 1970
 	timeBeforeRound = Date.now();
 	console.log(timeBeforeRound);
+	// Update user with timer repeatedly 
 	timer = setInterval( () => {
-		timePassed+=100;
+		timePassed = Date.now() - timeBeforeRound;
 		playerTimeEl.innerText = `${Math.floor(timePassed/1000)} : ${timePassed%1000}`;
-		// Update html element
-	}, 100 );
+	}, 10 );
 
-	// Add eventlistener for secret square
+	// --- Display the square to click ---
+
+	// Add eventlistener for secret square that the user has to click
+	// --- Change from game screen to the specific square --
 	gameScreenEl.addEventListener('click', stopTimer);
 }
 
+// Function to start up a new round
 const gameRound = (timeToWait) => {
+	// Hide waiting screen and display game screen
 	waitingScreenEl.classList.add('hide');
 	gameScreenEl.classList.remove('hide');
 	console.log("Starting timer " + timeToWait);
 
-	setTimeout(secretSquareClick, timeToWait);
+	// Wait before showing user wich square to click
+	setTimeout(startTimer, timeToWait);
 }
 
 // When another client connects
