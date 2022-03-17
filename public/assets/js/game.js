ed770ad678c7e693e8d3f3a4dbe298e1e8ad19a6
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-03-17 14:05:16 +0100

    Calculate timer for client

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -6,15 +6,56 @@ const startScreenEl = document.querySelector('#start-screen');
 const nameFormEl = document.querySelector('#name-form');
 const waitingScreenEl = document.querySelector('#waiting-screen');
 const gameScreenEl = document.querySelector('#game-screen');
+const playerTimeEl = document.querySelector('#userTime h5');
 
 // Username to identify client
 let username = null;
 let room = null;
 
-gameRound = (timeToWait) => {
+let timePassed = null;
+let timer = null;
+let timeBeforeRound = null;
+let timeAfterRound = null;
+
+const startTimer = () => {
+	timePassed = 0;
+	timer = setInterval( () => {
+		timePassed++;
+	}, 1 );
+}
+
+const stopTimer = () => {
+
+}
+
+const secretSquareClick = () => {
+	console.log("Wait time over, lets start!");
+
+	// set interval timer
+	timePassed = 0;
+	timeBeforeRound = Date.now();
+	console.log(timeBeforeRound);
+	timer = setInterval( () => {
+		timePassed+100;
+		playerTimeEl.innerText = `${Math.floor(timePassed/1000)} : ${timePassed%1000}`;
+		// Update html element
+	}, 100 );
+
+	// Add eventlistener for secret square
+	gameScreenEl.addEventListener('click', () => {
+		clearInterval(timer);
+		timeAfterRound = Date.now();
+		timePassed = timeAfterRound - timeBeforeRound;
+		console.log('It took you ' + timePassed + ' milliseconds to click');
+	});
+}
+
+const gameRound = (timeToWait) => {
 	waitingScreenEl.classList.add('hide');
 	gameScreenEl.classList.remove('hide');
-	console.log(timeToWait);
+	console.log("Starting timer " + timeToWait);
+
+	setTimeout(secretSquareClick, timeToWait);
 }
 
 // When another client connects
